f737fa541b8a3a0ad8e51fcab6306dd30a609dfc
Sherlock55561 <105907814+Sherlock55561@users.noreply.github.com>
2025-03-27 11:00:45 +1100

    Update __init__.py

--- LLaVA-main/llava/model/__init__.py
+++ LLaVA-main/llava/model/__init__.py
@@ -1,7 +1,6 @@
 try:
-   from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
-   from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
-   from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
-except Exception as e:
-  print("Import error:", e)
- raise
+    from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
+    from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
+    from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
+except:
+    pass
